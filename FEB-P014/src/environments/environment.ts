8cee5165442facf5045f6d547a1bfc4c73dea9d8
Daniel Monteiro <143518947+DanielMonteiroo@users.noreply.github.com>
2024-03-10 10:37:25 -0300

    finalizado

---
+++ FEB-P014/src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAyXfeWsT40scC592QmEQB2iyCv-Gi0vPA",
+    authDomain: "authfirebase-feb-p014-cad45.firebaseapp.com",
+    projectId: "authfirebase-feb-p014-cad45",
+    storageBucket: "authfirebase-feb-p014-cad45.appspot.com",
+    messagingSenderId: "148304608922",
+    appId: "1:148304608922:web:9b1308eb6860cd2931e923",
+    measurementId: "G-GT3R2NWK7F"
+  }
+};
+
