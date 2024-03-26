f40b4d63ecb276cb81872781cf77ec51c07bb66e
Daniel Monteiro <143518947+DanielMonteiroo@users.noreply.github.com>
2024-03-26 16:08:49 -0300

    atividade log

---
+++ semana02-aula02/cadastro/src/main/java/com/daniel/cadastro/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.daniel.cadastro.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome,email,senha;
+	
+}
