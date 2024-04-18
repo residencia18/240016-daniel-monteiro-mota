5bab77d7ff95fd5a4b4a38de487781165805ed31
Daniel Monteiro <143518947+DanielMonteiroo@users.noreply.github.com>
2024-04-18 15:22:14 -0300

    Prática Versionamento de API's

---
+++ aula-10/cadastro/src/main/java/com/daniel/cadastro/CadastroApplication.java
@@ -0,0 +1,31 @@
+package com.daniel.cadastro;
+
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.javafaker.Faker;
+
+@SpringBootApplication
+public class CadastroApplication {
+	public static final Logger log = LoggerFactory.getLogger(CadastroApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroApplication.class, args);
+		log.info("Aplicação em execução");
+		
+		//Gerando dados com FAKER
+		Faker fake = new Faker(new Locale("pt-BR"));	
+	    System.out.printf("\nNome: %s%n",fake.name().fullName());
+	    System.out.printf("Endereço: %s%n",fake.address().streetAddress());
+	    System.out.printf("Cidade: %s%n",fake.address().cityName());
+	    System.out.printf("País: %s%n",fake.address().country());
+	    System.out.printf("Telefone: %s%n",fake.phoneNumber().cellPhone());
+	    System.out.printf("Idade: %s%n",fake.date().birthday());
+	    System.out.printf("Email: %s%n",fake.internet().emailAddress());
+	    System.out.printf("Profissão: %s%n",fake.job().position());
+		
+	}
+
+}
---
+++ aula-10/cadastro/src/main/java/com/daniel/cadastro/controller/UsuarioControllerV1.java
@@ -0,0 +1,54 @@
+package com.daniel.cadastro.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniel.cadastro.model.Usuario;
+import com.daniel.cadastro.service.UsuarioServiceV1;
+
+@RestController
+@RequestMapping("/v1/usuarios")
+public class UsuarioControllerV1 {
+
+	@Autowired
+	private UsuarioServiceV1 usuarioService;
+
+	// Cria novo usuario
+	@PostMapping
+	public Usuario criarUsuario(@RequestBody @NonNull Usuario user) {
+		return usuarioService.criarUsuario(user);
+	}
+
+	// Lista todos usuarios
+	@GetMapping
+	public List<Usuario> listaUsuarios() {
+		return usuarioService.listaUsuarios();
+	}
+
+	// Busca pelo ID
+	@GetMapping("/{id}")
+	public Optional<Usuario> usuarioById(@PathVariable @NonNull Long id) {
+		return usuarioService.usuarioById(id);
+	}
+
+	// Atualiza usuario pelo ID
+	@PutMapping("/{id}")
+	public Usuario updateUsuario(@PathVariable @NonNull Long id, @RequestBody Usuario usuario) {
+		return usuarioService.updateUsuario(id, usuario);
+	}
+
+
+}
+
+
